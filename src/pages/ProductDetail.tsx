fc41394ab77c1910110ff4c33145ad28586ccdcc
JoabChua <chuaweeliang91@gmail.com>
2021-10-08 00:34:53 +0800

    add more info to product detail page

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,7 +1,9 @@
+import { ArrowLeftIcon } from "@heroicons/react/solid";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { CardDetail } from "../components/Card";
 import { CAROUSELITEMS } from "../data/dummy";
 import { AppDispatch, RootState } from "../store";
@@ -46,8 +48,17 @@ const ProductDetail = () => {
 
   return (
     <div className={classes.container}>
+      <Link className={classes.backlink} to="/product">
+        <ArrowLeftIcon className={classes.icon} />
+        Back to products
+      </Link>
       <div className={classes.item} key={item.id}>
-        <img src={item.img} alt={item.anchorText} width="200px" />
+        <img
+          src={item.img}
+          alt={item.anchorText}
+          width="200px"
+          height="200px"
+        />
         <div className={classes.desc}>
           <h4>{item.desc}</h4>
           <p className={classes.price}>Price: ${item.price}</p>
@@ -66,6 +77,29 @@ const ProductDetail = () => {
             <button className={classes.counter} onClick={increment}>
               +
             </button>
+          </div>
+          <hr />
+          <div className={classes.title}>About this item</div>
+          <ul>
+            <li>This is limited edition</li>
+            <li>1 year warranty</li>
+            <li>Free shipping</li>
+            <li>Discount voucher of $5 applies</li>
+          </ul>
+          <hr />
+          <div className={classes.additional}>
+            <div className={classes.row}>
+              <label>Customer Review</label>
+              <span>4.0 / 5.0 - 200 Ratings</span>
+            </div>
+            <div className={classes.row}>
+              <label>24/7 Support</label>
+              <span>Available in this region</span>
+            </div>
+            <div className={classes.row}>
+              <label>100 Days Money Guarantee</label>
+              <span>Refund in Credit</span>
+            </div>
           </div>
         </div>
       </div>
